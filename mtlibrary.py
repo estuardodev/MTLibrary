2909ba76c71a55cab7a960d881ef6e592f1a29d0
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-07-13 21:58:41 -0600

    Se arreglaron errores y se añadio una breve descripcion de cada funcion del modulo

--- mtlibrary.py
+++ mtlibrary.py
@@ -1,88 +1,124 @@
 '''
 Name: MTLibrary
 Versión: 2.0
-Versión-desarrollo: 2.0.1
+Versión-desarrollo: 2.0.4
 Description: MTLibrary es una libreria para Python para fácilitar las matemáticas en Python en español.
 Author: Estuardo Ramírez
 Social: @estuardodev | Twitter, GitHub, Instagram
 '''
 
-def suma(num, num2):
+def suma(dato1=0, dato2=0):
+    '''
+    SUMA:\n
+    Ejecutara una suma entre los valores asignados al primer argumento (dato1) y el segundo argumento (dato2)
+    '''
     try:
-        suma = num + num2
+        suma = dato1 + dato2
         return suma
     except TypeError:
         tipo = "suma"
         error = f"Se ha ingresado un valor invalido en la {tipo}"
         return error
 
-def resta(num, num2):
+def resta(dato1=0, dato2=0):
+    '''
+    RESTA:\n
+    Ejecutara una resta entre los valores asignados al primer argumento (dato1) y el segundo argumento (dato2)
+    '''
     try:
-        resta = num - num2
+        resta = dato1 - dato2
         return resta
     except TypeError:
         tipo = "resta"
         error = f"Se ha ingresado un valor invalido en la {tipo}"
         return error
 
-def multiplicacion(num, num2):
+def multiplicacion(dato1=0, dato2=0):
+    '''
+    MULTIPLICACIÓN:\n
+    Ejecutara una multiplicación en donde el segundo armugento (dato2) multiplicara al primer argumento (dato1)
+    '''
     try:
-        multiplicacion = num * num2
+        multiplicacion = dato1 * dato2
         return multiplicacion
     except TypeError:
         tipo = "multiplicación"
         error = f"Se ha ingresado un valor invalido en la {tipo}"
         return error
 
-def division(num, num2):
+def division(dato1=0, dato2=0):
+    '''
+    DIVISIÓN:\n
+    Ejecutara una división donde el primer argumento (dato1) sera dividido entre el segundo argumento (dato2)
+    '''
     try:
-        division = num / num2
+        division = dato1 / dato2
         return division
     except TypeError:
         tipo = "división"
         error = f"Se ha ingresado un valor invalido en la {tipo}"
         return error
 
-def residuo(num, num2):
+def residuo(dato1=0, dato2=0):
+    '''
+    RESIDUO:\n
+    Al ejecutarse devolvera el residuo de la división del primer argumento (dato1) dividido el segundo argumento (dato2)
+    '''
     try:
-        resultado = num % num2
+        resultado = dato1 % dato2
         return resultado
     except TypeError:
         tipo = "residuo"
         error = f"Se ha ingresado un valor invalido al obtener el {tipo}"
         return error
 
-def raiz(num):
+def raiz(dato1=0):
+    '''
+    RAÍZ:\n
+    Devolvera la raíz cuadrada del valor dado
+    '''
     try:
-        x = pow(num,0.5)
+        x = pow(dato1,0.5)
         return x
     except TypeError:
         tipo = ""
         error = f"Se ha ingresado un valor invalido en la {tipo}"
         return error
 
-def radio(num):
+def radio(dato1=0):
+    '''
+    RADIO:\n
+    Como resultado devolvera el radio de un circulo del valor dado
+    '''
     try:
-        radio = num / 2
+        radio = dato1 / 2
         return radio
     except TypeError:
-        tipo = ""
+        tipo = "radio"
         error = f"Se ha ingresado un valor invalido en la {tipo}"
         return error
 
-def area(num):
+def area(dato1=0):
+    '''
+    ÁREA:\n
+    Como resultado sera el área del valor dado en el argumento
+    '''
     try:
-        area = 3.141592653589793*num**2
+        area = 3.141592653589793*dato1**2
         return area
     except TypeError:
         tipo = "area"
         error = f"Se ha ingresado un diametro invalido para la función {tipo}"
         return error
 
-def porcentaje(num, num2):
+def porcentaje(dato1=0, dato2=0):
+    '''
+    PORCENTAJE:\n
+    Devolvera el porcentaje asignado en el segundo argumento (dato2) del primer argumento (dato1)
+    '''
     try:
-        num2 = num2 / 100
-        porcentaje = num * num2
+        dato2 = dato2 / 100
+        porcentaje = dato1 * dato2
         porcentaje = round(porcentaje, 2)
         return porcentaje
     except TypeError:
@@ -90,6 +126,16 @@ def porcentaje(num, num2):
         error = f"Se ha ingresado un valor invalido en el {tipo}"
         return error
 
-def potencia(num, num2):
-    resultado = num ** num2
-    return resultado
+def potencia(dato1=0, dato2=0):
+    '''
+    POTENCIA:\n
+    Potenciara el valor asignado en el primer argumento (dato1) al del valor asignado al segundo argumento (dato2)
+    '''
+    try:
+        resultado = dato1 ** dato2
+        return resultado
+    except TypeError:
+        tipo = "potencia"
+        error = f"Se ha ingresado un valor invalido en la {tipo}"
+        return error
+    
